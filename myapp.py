9130aa9721edeef3382dc2fee3d4e33b583ca407
moazzamriaz1 <98743892+moazzamriaz1@users.noreply.github.com>
2023-05-30 14:26:09 +0500

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,68 +1,3 @@
-'''
-import streamlit as st
-import langchain
-import os
-import openai
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-from langchain import OpenAI
-from langchain import VectorDBQA
-from langchain.document_loaders import UnstructuredFileLoader
-from langchain.text_splitter import CharacterTextSplitter
-import nltk
-
-
-
-nltk.download("punkt")
-
-def main():
-    st.title("Document Query App")
-
-    # File upload section
-    uploaded_file = st.file_uploader("Upload a document", type=["pdf"])
-    if uploaded_file is not None:
-        # Save the uploaded file
-        file_path = save_uploaded_file(uploaded_file)
-
-        # Load the file as document
-        loader = UnstructuredFileLoader(file_path)
-        documents = loader.load()
-
-        # Split the documents into texts
-        text_splitter = CharacterTextSplitter(chunk_size=800, chunk_overlap=0)
-        texts = text_splitter.split_documents(documents)
-
-        # Initialize OpenAIEmbeddings
-        openai_api_key = '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'
-        os.environ['OPENAI_API_KEY'] = openai_api_key
-
-        # Initialize OpenAIEmbeddings
-        embeddings = OpenAIEmbeddings(openai_api_key=os.environ['OPENAI_API_KEY'])
-
-        # Create Chroma vectorstore from documents
-        doc_search = Chroma.from_documents(texts, embeddings)
-
-        # Initialize VectorDBQA
-        chain = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type="stuff", vectorstore=doc_search)
-
-        # Prompt the user for a query
-        query = st.text_input("Enter your query")
-
-        if st.button("Run Query"):
-            # Run the query against the document
-            result = chain.run(query)
-            st.write(result)
-
-
-def save_uploaded_file(uploaded_file):
-    file_path = os.path.join("./uploaded_files", uploaded_file.name)
-    with open(file_path, "wb") as f:
-        f.write(uploaded_file.getbuffer())
-    return file_path
-
-if __name__ == "__main__":
-    main()
-'''
 
 import streamlit as st
 import os
@@ -90,7 +25,7 @@ def main():
             f.write(uploaded_file.read())
 
         # Initialize OpenAIEmbeddings
-        openai_api_key = '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'
+        openai_api_key = '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'
         os.environ['OPENAI_API_KEY'] = openai_api_key
 
         # Initialize OpenAIEmbeddings
